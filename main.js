0bbefb703f940dcec1e07341bba496656d470b52
Anupam Srivastava <anupam@dont.tech>
2019-01-03 15:43:25 +0530

    Singleton and Factory

--- main.js
+++ main.js
@@ -1,59 +1,8 @@
 (function () {
   'use strict';
 
-  // // Iterator function
-  // function nameIterator(names) {
-  //   let nextIndex = 0;
+  let imported = document.createElement('script');
+  imported.src = 'singleton.js';
+  document.head.appendChild(imported);
 
-  //   return {
-  //     next: function () {
-  //       return nextIndex < names.length ? {
-  //         value: names[nextIndex++],
-  //         done: false
-  //       } : {
-  //         done: true
-  //       };
-  //     }
-  //   };
-  // }
-
-  // const namesArr = ['anda', 'banda', 'danda'];
-  // const names = nameIterator(namesArr);
-  // console.log(names.next().value);
-  // console.log(names.next().value);
-  // console.log(names.next().value);
-  // console.log(names.next().value);
-  
-  // Generator function
-  function* sayNames() {
-    yield 'anda';
-    yield 'banda';
-    yield 'danda';
-  }
-
-  const name = sayNames();
-
-  console.log(name.next().value);
-  console.log(name.next().value);
-  console.log(name.next().value);
-  console.log(name.next().value);
-  
-  // ID creator
-  function* createIds() {
-    let index = 0;
-
-    while (true) {
-      yield index++;
-    }
-  }
-
-  const gen = createIds();
-  console.log(gen.next().value);
-  console.log(gen.next().value);
-  console.log(gen.next().value);
-  console.log(gen.next().value);
-  console.log(gen.next().value);
-  console.log(gen.next().value);
-  console.log(gen.next().value);
-  
 })();
